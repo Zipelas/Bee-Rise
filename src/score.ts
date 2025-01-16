eb9583f7f73797d4cc1b79030e7086df2d219e57
Rebecka94 <rebeckalarsson.1994@gmail.com>
2025-01-16 14:58:27 +0100

    Added a button class and a score class file with code

---
+++ src/score.ts
@@ -0,0 +1,31 @@
+class Score {
+    private color: string;
+    private currentHighScore: number;
+    private currentScore: number;
+    
+    constructor(color: string, currentHighScore: number, currentScore: number) {
+        this.color = color;
+        this.currentHighScore = currentHighScore;
+        this.currentScore = currentScore;
+    }
+
+    public update() {
+
+    }
+
+    public draw() {
+
+    } 
+
+    public displayScore() {
+
+    } 
+
+    public displayHighScore() {
+
+    }
+
+    public updateScore() {
+
+    }
+}
