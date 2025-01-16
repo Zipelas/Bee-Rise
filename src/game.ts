4123244f3ef195967369eb905ae0fa87809501ff
Rebecka94 <rebeckalarsson.1994@gmail.com>
2025-01-16 09:11:56 +0100

    Changing size of canvas board and removing music

--- src/game.ts
+++ src/game.ts
@@ -10,18 +10,10 @@ class Game {
   public update() {
     this.position.set(mouseX, mouseY);
     this.isCircleVisible = mouseIsPressed;
-
-    if (mouseIsPressed) {
-      if (!music.mystery.isPlaying()) {
-        music.mystery.loop();
-      }
-    } else {
-      music.mystery.pause();
-    }
   }
 
   public draw() {
-    background("black");
+    background("blue");
     this.drawText();
 
     if (this.isCircleVisible) {
