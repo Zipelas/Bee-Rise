6beabcfb4ffb6c13e0538822e27184ea93dcd799
Rebecka94 <rebeckalarsson.1994@gmail.com>
2025-01-17 15:37:03 +0100

    added draw and update method to gameworld

--- src/game.ts
+++ src/game.ts
@@ -2,7 +2,7 @@ class Game {
   private activeScene: Scene;
 
   constructor() {
-    this.activeScene = new StartMenu();
+    this.activeScene = new GameWorld();
   }
 
   public changeScene(nextScene: Scene) {
--- src/gameWorld.ts
+++ src/gameWorld.ts
@@ -1,15 +1,22 @@
-class GameWorld {
+class GameWorld implements Scene {
   private gameEntities: Entity[];
   private endOfGame: boolean;
   // private score: Score;
   // private pauseButton: Button;
-  private background: string;
+  // private background: string;
 
   constructor(gameEntities: Entity[], endOfGame: boolean, score: Score, pauseButton: Button, background: string) {
     this.gameEntities = gameEntities;
     this.endOfGame = endOfGame;
     // this.score = score;
     // this.pauseButton = pauseButton;
-    this.background = background; 
+    // this.background = background; 
+  }
+  update() {
+
+  }
+  draw() {
+    background("#2a9ec7");
+   
   }
 }
