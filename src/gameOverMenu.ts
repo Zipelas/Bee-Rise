ab3a1045e3a25ee39a846bdd6d2bbb66d2d79596
Andreas Fagerlund <mcshaffa@gmail.com>
2025-01-20 13:59:29 +0100

    Co-authored-by: Rebecka94 <Rebecka94@users.noreply.github.com>

--- src/gameOverMenu.ts
+++ src/gameOverMenu.ts
@@ -1,9 +1,37 @@
 class GameOverMenu implements Scene {
-    public update() {
-        
+    private reuseImage: p5.Im
+    private crossImage: p5.Image;
+  
+    constructor() {
+        this.reuseImage = loadImage("./assets/images/resuse.png");
+        this.crossImage = loadImage("./assets/images/cross.png");
+    }
+    
+    public update() {    
     }
 
     public draw() {
         background("#2a9ec7")
+        this.drawGameOverMenu()
+        this.crossImage()
+    }
+
+    private drawGameOverMenu() {
+        const backgroundX = width / 2 - 255;
+        const backgroundY = height / 2 + 10;
+        const backgroundWidth = 470;
+        const backgroundHeight = 250;
+        const cornerRadius = 150;
+        
+        fill("#FF9999"); 
+        noStroke();
+        rect(
+            backgroundX,
+            backgroundY,
+            backgroundWidth,
+            backgroundHeight,
+            cornerRadius,
+          
+        );
     }
 }
