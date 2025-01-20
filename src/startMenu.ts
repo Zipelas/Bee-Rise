fefb2f0c76524565c0d0d1cb6a7eb96c18ac518f
malishaah <melisarsh@gmail.com>
2025-01-20 09:27:23 +0100

    font-background-instruction

--- src/startMenu.ts
+++ src/startMenu.ts
@@ -1,10 +1,35 @@
 class StartMenu implements Scene {
+  private backgroundImage: p5.Image;
+  private arrowImage: p5.Image;
+
+  constructor() {
+    this.backgroundImage = loadImage("./assets/music/background.jpg");
+    this.arrowImage = loadImage("./assets/arrow-instructions.png");
+  }
+
     public update() {
-        
+        // Uppdateringslogik
     }
     public draw() {
-      background("blue");
-      // image(backgroundImage, 0, 0, width, height)
+      image(this.backgroundImage, 0, 0, width, height);
+
+    textFont("Modak");
+    stroke(0);
+    strokeWeight(8);
+    this.drawTextWithLetterSpacing(
+      "Bee Rise",
+      width / 2 - 300,
+      height / 4,
+      150,
+      10,
+      "#ffca00"
+    );
+  
+    // this.drawPlayButton();
+  
+    this.drawInstructions();
+  }
+  
     }
   
 }
