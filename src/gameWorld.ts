48845b1d55489b272bcb469573dd1180ae12dd96
Andreas Fagerlund <mcshaffa@gmail.com>
2025-01-22 16:32:29 +0100

    created creatRandomEnemy and added it to constructor

--- src/gameWorld.ts
+++ src/gameWorld.ts
@@ -6,12 +6,19 @@ class GameWorld implements Scene {
   // private background: string;
 
   constructor() {
-    this.gameEntities = [new Player()];
+    this.gameEntities = [new Player(), this.createRandomEnemy()];
+    // new Enemy("bird"), new Enemy("ufo"), new Enemy("plane")
     // this.endOfGame = endOfGame;
     // this.score = score;
     // this.pauseButton = pauseButton;
     // this.background = background; 
   }
+
+  private createRandomEnemy(): Enemy {
+    const types: ("bird" | "ufo" | "plane")[] = ["bird", "ufo", "plane"];
+    const randomType = random(types); // Slumpa en typ
+    return new Enemy(randomType);
+}
   
   update() {
     for (const gameEntitie of this.gameEntities) {
