e81d7b66782fc89ae725a5bcb38508e064006076
Sumaya-yusuf-1 <maxabahallah@gmail.com>
2025-01-23 17:26:12 +0100

    create cloud and score

--- src/button.ts
+++ src/button.ts
@@ -1,33 +1,3 @@
 class Button {
-    private title: string;
-    private backgroundColor: string;
-    private width: number;
-    private height: number;
-    private textColor: string;
-    private image: p5.Image;
-    private x: number;
-    private y: number;
-
-    constructor(title: string, backgroundColor: string, width: number, height: number, textColor: string, image: p5.Image, x: number, y: number) {
-        this.title = title;
-        this.backgroundColor = backgroundColor;
-        this. width = width;
-        this.height = height;
-        this.textColor = textColor;
-        this.image = image;
-        this.x = x;
-        this.y = y;
+    
         }
-
-        public update() {
-
-        }
-
-        public draw() {
-
-        }
-
-        public onclick() {
-            
-        }
-}
